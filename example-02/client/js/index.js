b8e7d75791ead582b8136baaa38dfd3788931583
Rob Brennan <rob@therobbrennan.com>
2021-05-03 14:53:33 -0700

    Annotate the example JavaScript file with notes from the blog post

--- example-02/client/js/index.js
+++ example-02/client/js/index.js
@@ -1,9 +1,10 @@
 ;(function () {
   "use strict"
 
+  // Define the message types that the user can expect to receive
   const MESSAGE_TYPE = {
-    SDP: "SDP",
-    CANDIDATE: "CANDIDATE",
+    SDP: "SDP", // Offers and answers
+    CANDIDATE: "CANDIDATE", // ICE candidates
   }
 
   document.addEventListener("click", async (event) => {
@@ -14,17 +15,25 @@
 
   const startChat = async () => {
     try {
+      // Request data from the camera and microphone
       const stream = await navigator.mediaDevices.getUserMedia({
         audio: true,
         video: true,
       })
+
+      // Once the user has granted permission, we can show the chat room
       showChatRoom()
 
+      // Establish a connection to our WebSocket server
       const signaling = new WebSocket("ws://127.0.0.1:1337")
-      const peerConnection = createPeerConnection(signaling)
+      const peerConnection = createPeerConnection(signaling) // NOTE: We are using a fake STUN server for this example.
 
+      // After creating the RTCPeerConnection object, we need to handle receiving a message
       addMessageHandler(signaling, peerConnection)
 
+      // Set our local tracks to the RTCPeerConnection object and display them in the desired video element
+      //  NOTE: Setting the tracks on the peer connection object will trigger the negogationneeded event, and the event listener
+      //  will call the createAndSendOffer function.
       stream
         .getTracks()
         .forEach((track) => peerConnection.addTrack(track, stream))
@@ -36,13 +45,16 @@
 
   const createPeerConnection = (signaling) => {
     const peerConnection = new RTCPeerConnection({
-      iceServers: [{ urls: "stun:stun.l.test.com:19000" }],
+      iceServers: [{ urls: "stun:stun.l.test.com:19000" }], // NOTE: This is a fake STUN server. You can replace it with a public one.
     })
 
+    // This event is super important. It is fired when we add tracks to the connection, and later when something happens that requires a renegotiation.
+    //    TL:DR This event is where the signaling exchange will actually get started.
     peerConnection.onnegotiationneeded = async () => {
       await createAndSendOffer()
     }
 
+    // Send ICE candidates to the peers
     peerConnection.onicecandidate = (iceEvent) => {
       if (iceEvent && iceEvent.candidate) {
         signaling.send(
@@ -54,6 +66,7 @@
       }
     }
 
+    // Set the received tracks to our video HTML element
     peerConnection.ontrack = (event) => {
       const video = document.getElementById("remote-view")
       if (!video.srcObject) {
@@ -75,9 +88,15 @@
       const { message_type, content } = data
       try {
         if (message_type === MESSAGE_TYPE.CANDIDATE && content) {
+          // If we receive a candidate, give it to the ICE agent
           await peerConnection.addIceCandidate(content)
         } else if (message_type === MESSAGE_TYPE.SDP) {
           if (content.type === "offer") {
+            // If we receive an offer:
+            //  - Set the remote description
+            //  - Create an answer
+            //  - Save the answer as a local description
+            //  - Send it to the peer
             await peerConnection.setRemoteDescription(content)
             const answer = await peerConnection.createAnswer()
             await peerConnection.setLocalDescription(answer)
@@ -88,6 +107,8 @@
               })
             )
           } else if (content.type === "answer") {
+            // If we receive an answer:
+            //  - Set it as the local offer
             await peerConnection.setRemoteDescription(content)
           } else {
             console.log("Unsupported SDP type.")
